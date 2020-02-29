c04d5004d670e42e87182b5c4712727a7ba0c56d
Arges86 <st343584@gmail.com>
2020-02-29 12:23:01 -0500

    updated to use axios instead of rp
    changed RESTful call to use same package

--- server/commons/webpage.js
+++ server/commons/webpage.js
@@ -1,5 +1,6 @@
-const rp = require("request-promise");
+// const rp = require("request-promise");
 const cheerio = require("cheerio");
+const axios = require("axios");
 
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
@@ -11,70 +12,67 @@ async function getSite(data, dictionary) {
 
   const domain = breakDownURL(data);
 
-  const options = {
-    uri: data,
-    transform: function (body) {
-      return cheerio.load(body);
-    }
-  };
-
   const start = process.hrtime.bigint();
-  return rp(options)
-    .then($ => {
-      console.log($("title").text());
-      var end = process.hrtime.bigint();
+  
+  let html;
+  try {
+    html = await axios.get(data);
+  } catch (error) {
+    throw new Error("Failed to resolve page");
+  }
+  
+  const $ = await cheerio.load(html.data);
 
-      let textArray = ($("body").text()).replace(/\W/g, " "); // removes all non 'word characters'
-      textArray = textArray.split(" "); // splits text into array at space
-      textArray = textArray.filter(function (e) { return e; });
-      textArray = uniq(textArray); // removes any duplicate words
-      textArray = textArray.filter(x => isNaN(x)); // removes any numbers
+  console.log($("title").text());
+  const end = process.hrtime.bigint();
 
-      //loops through text and spell checks
-      const correct = [], incorrect=[];
-      textArray.forEach(element => {
-        if (inDictionary(dictionary, element)) {
-          correct.push(element);
-        } else {
-          incorrect.push(element);
+  let textArray = ($("body").text()).replace(/\W/g, " "); // removes all non 'word characters'
+  textArray = textArray.split(" "); // splits text into array at space
+  textArray = textArray.filter(function (e) { return e; });
+  textArray = uniq(textArray); // removes any duplicate words
+  textArray = textArray.filter(x => isNaN(x)); // removes any numbers
+
+  //loops through text and spell checks
+  const correct = [], incorrect=[];
+  textArray.forEach(element => {
+    if (inDictionary(dictionary, element)) {
+      correct.push(element);
+    } else {
+      incorrect.push(element);
+    }
+  });
+
+  // creates url
+  var urlArray = [];
+  $("a").each(function () {
+    let uri = $(this).attr("href");
+
+    if (uri === undefined || uri === null) {
+      // do nothing
+    } else {
+
+      // if url is part of the domain
+      if (breakDownURL(uri) === domain) {
+        // if URI ends with with a backslash, remove it
+        if (uri.endsWith("/")) {
+          uri = uri.substring(0, uri.length - 1);
         }
-      });
+        urlArray.push(uri.toLowerCase().trim());
+      }
+    }
+  });
 
-      // creates url
-      var urlArray = [];
-      $("a").each(function () {
-        let uri = $(this).attr("href");
+  // gets the number of seconds of elapsed time
+  const seconds = ((Number(end - start)) / 1000000000).toFixed(3);
 
-        if (uri === undefined || uri === null) {
-          // do nothing
-        } else {
-
-          // if url is part of the domain
-          if (breakDownURL(uri) === domain) {
-            // if URI ends with with a backslash, remove it
-            if (uri.endsWith("/")) {
-              uri = uri.substring(0, uri.length - 1);
-            }
-            urlArray.push(uri.toLowerCase().trim());
-          }
-        }
-      });
-
-      // gets the number of seconds of elapsed time
-      const seconds = ((Number(end - start)) / 1000000000).toFixed(3);
-
-      const output = {
-        text: textArray,
-        incorrect: incorrect,
-        correct: correct,
-        links: urlArray,
-        time: `${seconds} seconds`,
-      };
-      return output;
-    })
-    .catch(() => {
-      throw new Error("Failed to resolve page");
-    });
+  const output = {
+    text: textArray,
+    incorrect: incorrect,
+    correct: correct,
+    links: urlArray,
+    time: `${seconds} seconds`,
+  };
+  return output;
 }
 
 async function getUrl(first, data, ws, dictionary) {
